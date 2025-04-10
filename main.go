990c24d68c343c2b7eae756472bf17833a309fa3
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-04-10 08:18:07 -0300

    vetor de soma

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func main() {
+	var numero int
+	var numero02 int
+   var numero03 int
+   var numero04 int
+   var numero05 int
+
+	fmt.Print("Digite um numero: ")
+	fmt.Scan(&numero)
+	fmt.Print("Digite outro numero: ")
+	fmt.Scan(&numero02)
+   fmt.Print("Digite um numero: ")
+	fmt.Scan(&numero03)
+   fmt.Print("Digite um numero: ")
+	fmt.Scan(&numero04)
+   fmt.Print("Digite um numero: ")
+	fmt.Scan(&numero05)
+	resultado := numero + numero02 + numero03 + numero04 + numero05
+	fmt.Printf("A soma dos numeros é: %d\n", resultado)
+}
+
+
+
+
+
+
  
- func main() {
-    // Slice
-   
-    nomes := [4]string{"joao", "alice", "lucas", "ruth"}
-   
-    fmt.Println(nomes)
-    rangeOne := nomes[:2]
-    fmt.Println(rangeOne)
-    rangeTwo := nomes[2:]
-    fmt.Println(rangeTwo)
-    rangeThree := nomes[1:3]
-    fmt.Println(rangeThree)
-   
- }
