80c24bc2a721af755b74f6ef50ffec06df87824b
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-04-10 08:40:30 -0300

    vetor de soma

--- main.go
+++ main.go
@@ -5,29 +5,20 @@ import (
 )
 
 func main() {
-	var numero int
-	var numero02 int
-   var numero03 int
-   var numero04 int
-   var numero05 int
+	var numeros [5]int 
 
-	fmt.Print("Digite um numero: ")
-	fmt.Scan(&numero)
-	fmt.Print("Digite outro numero: ")
-	fmt.Scan(&numero02)
-   fmt.Print("Digite um numero: ")
-	fmt.Scan(&numero03)
-   fmt.Print("Digite um numero: ")
-	fmt.Scan(&numero04)
-   fmt.Print("Digite um numero: ")
-	fmt.Scan(&numero05)
-	resultado := numero + numero02 + numero03 + numero04 + numero05
-	fmt.Printf("A soma dos numeros é: %d\n", resultado)
+	for i := 0; i < len(numeros); i++ {
+		fmt.Printf("Digite o %dº número: ", i+1)
+		fmt.Scan(&numeros[i])
+	}
+
+	soma :=0
+   
+	for i := 0; i < len(numeros); i++ {
+		soma += numeros[i]
+	}
+
+	fmt.Printf("A soma dos números é: %d\n", soma)
 }
-
-
-
-
-
 
  
