721b18f0b9856a1f14764dfe8fbff648830ad8c1
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-04-10 13:11:14 -0300

    saldo

--- main.go
+++ main.go
@@ -5,15 +5,27 @@ import (
 )
 
 func main() {
-	var idade int
-	fmt.Println("Informe sua idade")
-	fmt.Scan(&idade)
+	var saldo float32
+	var saque float32
+	var deposito float32
+	var decisao int
+	var saldofinal float32
+	var depositofinal float32
+	fmt.Println("Informe seu saldo")
+	fmt.Scan(&saldo)
+	fmt.Println("Para sacar digite 1 ou digite 2 para depositar")
+	fmt.Scan(&decisao)
 
-	if idade < 18 {
-		fmt.Println("você é menor de idade")
-	} else if idade >=18 && idade <60 {
-		fmt.Println("você é adulto")
-	} else if idade >=60 {
-		fmt.Println("voce é idoso")
-}
-}
+	if decisao == 1 {
+		fmt.Println("Valor desejado para sue saque:")
+		fmt.Scan(&saque)
+		saldofinal = saldo - saque
+		fmt.Println("seu saldo final é ",saldofinal)
+
+		} else if decisao == 2 {
+		fmt.Println("Valor desejado para deposito:")
+		fmt.Scan(&deposito)
+		depositofinal = saldo + deposito
+		fmt.Println("Seu saldo final é ", depositofinal)
+		}
+	}
