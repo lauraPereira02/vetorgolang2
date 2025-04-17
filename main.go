25aeaa83f50de83fefbb63571acdbf5acb6b16f0
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-04-17 09:27:36 -0300

    dadospessoa

--- main.go
+++ main.go
@@ -3,41 +3,14 @@ package main
 import (
 	"fmt"
 )
-
-var saldo float32
-var valor float32
-
-func main() {
-	var opcao int
-
-	fmt.Print("Informe seu saldo inicial: ")
-	fmt.Scan(&saldo)
-	fmt.Println("Escolha uma opção: 1 para sacar, 2 para depositar")
-	fmt.Print("Opção: ")
-	fmt.Scan(&opcao)
-
-	fmt.Print("Informe o valor:")
-	fmt.Scan(&valor)
-
-	if opcao == 1 {
-		sacar()
-	} else if opcao == 2 {
-		depositar()
-	} else {
-		fmt.Println("Opção inválida.")
-	} 
-} 
-
-func sacar() {
-	if valor > saldo {
-		fmt.Println("Saldo insuficiente.")
-	} else {
-		saldo -= valor
-		fmt.Println("Saque realizado com sucesso! Saldo atual:", saldo)
+func dadosPessoa(idade int) (int, string) {
+	valor := "menor de idade"
+	if idade >= 18 {
+		valor = "maior de idade"
 	}
+	return idade, valor
 }
-
-func depositar() {
-	saldo += valor
-	fmt.Println("Depósito realizado com sucesso! Saldo atual:", saldo)
+func main(){
+	idade, condicao:= dadosPessoa(20)
+	fmt.Println(idade, condicao)
 }
