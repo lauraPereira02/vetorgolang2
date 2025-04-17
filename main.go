7cd19dddce07c5ab9f54821a9d6a4009de2eb751
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-04-17 11:04:14 -0300

    NOTAS E IDADE

--- main.go
+++ main.go
@@ -3,14 +3,28 @@ package main
 import (
 	"fmt"
 )
-func dadosPessoa(idade int) (int, string) {
-	valor := "menor de idade"
-	if idade >= 18 {
-		valor = "maior de idade"
+
+func main() {
+	// Mapa de idades
+	AlunoIdade := make(map[string]int)
+	AlunoIdade["Laura"] = 16
+	AlunoIdade["Fabiano"] = 40
+	AlunoIdade["Rauanny"] = 17
+	AlunoIdade["Livia"] = 16
+	AlunoIdade["Isabela"] = 15
+
+	fmt.Println("Idade da Laura:", AlunoIdade["Laura"])
+
+	// Mapa de notas
+	notasAlunos := map[string]float64{
+		"Laura":   8.5,
+		"Fabiano": 10,
+		"Rauanny": 7.4,
+		"Isabela": 6.8,
+		"Livia":   9.3,
 	}
-	return idade, valor
-}
-func main(){
-	idade, condicao:= dadosPessoa(20)
-	fmt.Println(idade, condicao)
+
+	for nome, nota := range notasAlunos {
+		fmt.Printf("%s tirou a nota %.2f\n", nome, nota)
+	}
 }
