03ca1a320f2ca1362749f482f72a12a806dbc883
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-04-24 14:00:20 -0300

    analizarnotas

--- main.go
+++ main.go
@@ -4,27 +4,19 @@ import (
 	"fmt"
 )
 
+func analisarNotas(nota1, nota2 float64) (float64, string) {
+	var media float64 = (nota1 + nota2) / 2
+	var resultado string
+
+	if media >= 6 {
+		resultado = "Aprovado"
+	} else {
+		resultado = "Reprovado"
+	}
+	return media, resultado
+}
 func main() {
-	// Mapa de idades
-	AlunoIdade := make(map[string]int)
-	AlunoIdade["Laura"] = 16
-	AlunoIdade["Fabiano"] = 40
-	AlunoIdade["Rauanny"] = 17
-	AlunoIdade["Livia"] = 16
-	AlunoIdade["Isabela"] = 15
-
-	fmt.Println("Idade da Laura:", AlunoIdade["Laura"])
-
-	// Mapa de notas
-	notasAlunos := map[string]float64{
-		"Laura":   8.5,
-		"Fabiano": 10,
-		"Rauanny": 7.4,
-		"Isabela": 6.8,
-		"Livia":   9.3,
-	}
-
-	for nome, nota := range notasAlunos {
-		fmt.Printf("%s tirou a nota %.2f\n", nome, nota)
-	}
+	media, resultado := analisarNotas(6, 5)
+	fmt.Println("Média:", media)
+	fmt.Println("Resultado:", resultado)
 }
