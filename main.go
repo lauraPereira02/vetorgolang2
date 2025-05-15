99fa5941b52c9227e3f7c363553180188644ef71
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-05-15 08:46:48 -0300

    jogador

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-	estoque := map[string]int{
-		"COXINHA": 10,
-		"PÃO DE QUEIJO": 15,
-		"REFRIGERANTE": 20,
-	}
-	estoque["COXINHA"] -= 2
-	estoque["PÃO DE QUEIJO"] -= 1
-	for produto, quantidade := range estoque {
-		fmt.Printf("%s: %d uni. restantes\n", produto, quantidade)
-	}
+type carro struct{
+	marca string
+	ano int
+	velocidade int
 }
+ 
+func mostrarDados(c carro){
+	fmt.Println("marca do carro: ", c.marca)
+	fmt.Println("ano do carro: ", c.ano)
+	fmt.Println("velocidade do carro: ", c.velocidade)
+}
+
+func main() {
+	c1 := carro {"ford", 2020, 190}
+	mostrarDados(c1)
+}
+
+
